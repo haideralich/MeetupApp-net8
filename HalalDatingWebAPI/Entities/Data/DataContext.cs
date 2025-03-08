358d9818a48f613b64ff04d7595e5904a3582c12
Haider Ali Chaudhary <haideralichoudhary@gmail.com>
2025-03-08 04:42:43 +0300

    Add project files.

---
+++ HalalDatingWebAPI/Entities/Data/DataContext.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+
+namespace HalalDatingWebAPI.Entities.Data
+{
+    public class DataContext(DbContextOptions options) : DbContext(options)
+    {
+        public DbSet<AppUser> Users { get; set; }
+    }
+}
